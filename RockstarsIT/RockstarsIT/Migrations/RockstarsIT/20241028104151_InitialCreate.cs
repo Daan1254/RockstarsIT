ec8d523d56ca81c9010ec44c410bda6b552e42f9
Cassandra Khaya <i507626@fontysict.nl>
2024-10-28 15:09:47 +0100

    CRUD toegepast

---
+++ RockstarsIT/RockstarsIT/Migrations/RockstarsIT/20241028104151_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RockstarsIT.Migrations.RockstarsIT
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Squads",
+                columns: table => new
+                {
+                    name = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Squads", x => x.name);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Squads");
+        }
+    }
+}
--- RockstarsIT/RockstarsIT/Models/Squads.cs
+++ RockstarsIT/RockstarsIT/Models/Squads.cs
@@ -1,7 +1,11 @@
-﻿namespace RockstarsIT.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RockstarsIT.Models
 {
+    
     public class Squads
     {
+        [Key]
         public string name { get; set; }
         public string description { get; set; }
     }
