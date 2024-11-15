ec5f5d956a2911e0d3faed5e2d05494e01df8835
Daan Verbeek <daanverbeek13@gmail.com>
2024-11-15 11:36:02 +0100

    Fixed squad crud

---
+++ RockstarsIT/RockstarsIT_BLL/Interfaces/ISquadRepository.cs
@@ -0,0 +1,14 @@
+using RockstarsIT_BLL.Dto;
+
+namespace RockstarsIT_BLL.Interfaces;
+
+public interface ISquadRepository
+{
+    public List<SquadDto> GetAllSquads();
+    public SquadDto GetSquadById(int id);
+    public bool CreateSquad(CreateEditSquadDto squadDto);
+    
+    public bool EditSquad(int id, CreateEditSquadDto squadDto);
+    
+    public bool DeleteSquad(int id);
+}
--- RockstarsIT/RockstarsIT_BLL/SquadService.cs
+++ RockstarsIT/RockstarsIT_BLL/SquadService.cs
@@ -1,16 +1,39 @@
 using RockstarsIT_BLL.Dto;
+using RockstarsIT_BLL.Interfaces;
 
 namespace RockstarsIT_BLL;
 
 public class SquadService
 {
-    public SquadService()
+    private readonly ISquadRepository _squadRepository;
+    
+    public SquadService(ISquadRepository squadRepository)
     {
-        
+        _squadRepository = squadRepository;
     }
     
     public List<SquadDto> GetSquads()
     {
-        return new List<SquadDto>();
+        return _squadRepository.GetAllSquads();
+    }
+    
+    public SquadDto GetSquadById(int id)
+    {
+        return _squadRepository.GetSquadById(id);
+    }
+    
+    public bool CreateSquad(CreateEditSquadDto squadDto)
+    {
+        return _squadRepository.CreateSquad(squadDto);
+    }
+    
+    public bool EditSquad(int id, CreateEditSquadDto squadDto)
+    {
+        return _squadRepository.EditSquad(id, squadDto);
+    }
+    
+    public bool DeleteSquad(int id)
+    {
+        return _squadRepository.DeleteSquad(id);
     }
 }
