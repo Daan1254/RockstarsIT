d0baac5cc8803b0b160f835df82bd5d3aa4a6055
Daan Verbeek <daanverbeek13@gmail.com>
2024-12-16 14:29:39 +0100

    Fixed sending emails to surveys

--- RockstarsIT/RockstarsIT/Controllers/SurveyController.cs
+++ RockstarsIT/RockstarsIT/Controllers/SurveyController.cs
@@ -11,14 +11,16 @@ public class SurveyController : Controller
     private readonly SurveyService _surveyService;
     private readonly QuestionService _questionService;
     private readonly SquadService _squadService;
+    private readonly EmailService _emailService;
 
 
     // Injecting DbContext in the constructor
-    public SurveyController(SurveyService surveyService, QuestionService questionService, SquadService squadService)
+    public SurveyController(SurveyService surveyService, QuestionService questionService, SquadService squadService, EmailService emailService)
     {
         _surveyService = surveyService;
         _questionService = questionService;
         _squadService = squadService;
+        _emailService = emailService;
     }
     
     // GET
@@ -67,39 +69,9 @@ public class SurveyController : Controller
         return View(surveyViewModel);
     }
 
-        public IActionResult SendEmail(int id) {
-        // SurveyViewModel? survey = _context.Surveys.Find(id);
-        //
-        // // Define sender and recipient email addresses
-        // string host = DotEnv.Read()["BRAVO_SMTP_HOST"];
-        // string port = DotEnv.Read()["BRAVO_SMTP_PORT"];
-        // string fromEmail = DotEnv.Read()["BRAVO_SMTP_FROM_EMAIL"];
-        // string userName = DotEnv.Read()["BRAVO_SMTP_USERNAME"];
-        // string password = DotEnv.Read()["BRAVO_SMTP_PASSWORD"];
-        // string toEmail = "daanverbeek15@gmail.com";
-        //
-        // // Email message setup
-        // var mail = new MailMessage();
-        // mail.From = new MailAddress(fromEmail);
-        // mail.To.Add(toEmail);
-        // mail.Subject = $"U bent uitgenodigd voor de {survey?.Title} enquête!";
-        // mail.Body = "Beste, <br> U bent uitgenodigd om de volgende enquête in te vullen: <br> <br> " +
-        //             $"<b>{survey?.Title}</b> <br> {survey?.Description} <br> <br> " +
-        //             "Klik op de volgende link om de enquête in te vullen: <br> " +
-        //             "<a href='http://localhost:5169/Survey/'>Klik hier om de enquête in te vullen</a>";
-        //
-        // mail.IsBodyHtml = true; // Make sure to set this so the HTML renders properly
-        //
-        // // Configure SMTP client
-        // using (var smtp = new SmtpClient(host, int.Parse(port)))
-        // {
-        //     smtp.Credentials = new NetworkCredential(userName, password);
-        //     smtp.Send(mail);
-        // }
-        //Template maken voor de emails. Dat er automatisch iets is ingevuld.
-        //Het mag geen page zijn.
-        //Denkt dat file/ template niet in presentation layer staat. 
-        return View("Details", new FullSurveyViewModel()); 
+    public IActionResult SendEmail(int surveyId) {
+        _emailService.SendEmails(surveyId);
+        return RedirectToAction("Details", new { id = surveyId });
     } 
 
     public IActionResult Create()
